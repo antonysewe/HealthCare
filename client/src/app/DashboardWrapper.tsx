93fed44744c8df86cc067ee2eab787229301d717
Antony-Sewe <antonywing6@gmail.com>
2025-11-06 22:57:34 +0300

    first commit

---
+++ client/src/app/DashboardWrapper.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import React, { useEffect } from 'react'
+import Navbar from '../components/NavBar'
+import Sidebar from '../components/SideBar'
+
+{/* state provider? */}
+{/* This is redux state/Storeprovider provider */}
+import StoreProvider, { useAppSelector } from './redux';
+
+
+const DashboardLayout = ({children}:{children: React.ReactNode}) => {
+
+  {/* Feature 1. Is sidebar collapsed */}
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+
+  {/* Feature 2. DarkMode */}
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  useEffect(() =>{
+    if (isDarkMode){
+      document.documentElement.classList.add('dark');
+    } else{
+      document.documentElement.classList.remove('dark');
+    }
+  });
+
+  return (
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+
+      <Sidebar/>
+      {/* not sure if its checking darkmode */}
+      <main className={` flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${isSidebarCollapsed ? "": "md:pl-64"}`}>
+        <Navbar/>
+        {children}
+      </main>
+    </div>
+  )
+};
+
+const DashboardWrapper = ({children}:{children: React.ReactNode}) => {
+  return(
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
+  )
+}
+
+export default DashboardWrapper
