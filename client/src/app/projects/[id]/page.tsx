7e2abc1009418206d38d139dd2b9563ad33981fb
Antony-Sewe <antonywing6@gmail.com>
2025-12-04 10:14:02 +0300

    changes to projects types: amplify client server type rule

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -1,44 +1,15 @@
-"use client";
+import Project from "./project";
 
-import React, { useState } from "react";
-import ProjectHeader from "@/app/projects/ProjectHeader";
-import Board from "../BoardView";
-import List from "../ListView";
-import Timeline from "../TimelineView";
-import Table from "../TableView";
-import ModalNewTask from "@/components/ModalNewTask";
+import React from 'react'
 
-type Props = {
-  params: { id: string };
-};
-
-const Project = ({ params }: Props) => {
-  const { id } = params;
-  const [activeTab, setActiveTab] = useState("Board");
-  const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
+async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
 
   return (
     <div>
-      <ModalNewTask
-        isOpen={isModalNewTaskOpen}
-        onClose={() => setIsModalNewTaskOpen(false)}
-        id={id}
-      />
-      <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
-      {activeTab === "Board" && (
-        <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
-      )}
-      {activeTab === "List" && (
-        <List id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
-      )}
-      {activeTab === "Timeline" && (
-        <Timeline id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
-      )}
-      {activeTab === "Table" && (
-        <Table id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
-      )}
+      <Project id={id} />
     </div>
   );
-};
+}
 
-export default Project;
+export default Page
